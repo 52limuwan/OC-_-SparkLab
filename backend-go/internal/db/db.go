798eb243b5e5e08668bef7fcda03ee7042f17cd8
shenyi <52limuwan@gmail.com>
2026-03-30 22:45:19 +0800

    12

--- backend-go/internal/db/db.go
+++ backend-go/internal/db/db.go
@@ -1,10 +1,32 @@
 package db
 
 import (
+	"bigdata_zhoc/backend-go/internal/model"
+
 	"github.com/glebarez/sqlite"
 	"gorm.io/gorm"
 )
 
 func Open(path string) (*gorm.DB, error) {
-	return gorm.Open(sqlite.Open(path), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(path), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.AutoMigrate(
+		&model.User{},
+		&model.Course{},
+		&model.Lab{},
+		&model.Step{},
+		&model.Enrollment{},
+		&model.Submission{},
+		&model.Container{},
+		&model.Snapshot{},
+		&model.Server{},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
